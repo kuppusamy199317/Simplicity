c05685328a1e08c4d43682f4a22a69eea1474641
kuppusamy <kuppusamykaliyappan@gmail.com>
2017-09-07 11:27:03 +0530

    changes in main page

--- app/src/main/java/info/androidhive/materialdesign/activity/MainActivity.java
+++ app/src/main/java/info/androidhive/materialdesign/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
-
+        Log.e("CHECK","GIT TEST");
         // display the first navigation drawer view on app launch
         displayView(0);
     }
